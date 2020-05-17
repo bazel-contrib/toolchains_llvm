f4c17a3ae40f927ff62cc0fb8fe22b1530871807
Gaurav Sharma <sgauravsharma059@gmail.com>
2020-05-16 23:32:23 -0400

    Support Ubuntu 20.04 (#47)
    Use the same distribution as Ubuntu 18.04.

--- toolchain/tools/llvm_release_name.py
+++ toolchain/tools/llvm_release_name.py
@@ -71,6 +71,9 @@ def _linux(llvm_version):
         os_name = "linux-gnu-ubuntu-14.04"
     elif (distname == "ubuntu" and version.startswith("18.04")) or (distname == "linuxmint" and version.startswith("19")):
         os_name = "linux-gnu-ubuntu-18.04"
+    elif (distname == "ubuntu" and version.startswith("20")):
+        # use ubuntu 18.04 clang LLVM release for ubuntu 20.04
+        os_name = "linux-gnu-ubuntu-18.04"
     elif distname in ["arch", "ubuntu", "manjaro"] or (distname == "linuxmint" and version.startswith("18")):
         os_name = "linux-gnu-ubuntu-16.04"
     elif distname == "debian" and (version is None or int(version) == 10):
