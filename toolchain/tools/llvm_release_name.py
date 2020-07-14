3dcfe93d1a765da769c9f15ca38e3d12af536e8d
Yaz Saito <yasushi.saito@gmail.com>
2020-07-14 12:28:45 -0700

    Support Ubuntu 20.04.

--- toolchain/tools/llvm_release_name.py
+++ toolchain/tools/llvm_release_name.py
@@ -69,6 +69,10 @@ def _linux(llvm_version):
         os_name = "linux-sles%s" % version
     elif distname == "ubuntu" and version.startswith("14.04"):
         os_name = "linux-gnu-ubuntu-14.04"
+    elif (distname == "ubuntu" and version.startswith("20.04")) or (distname == "linuxmint" and version.startswith("20")):
+        # There is no binary packages specifically for 20.04, but those for 18.04 works on
+        # 20.04
+        os_name = "linux-gnu-ubuntu-18.04"
     elif (distname == "ubuntu" and version.startswith("18.04")) or (distname == "linuxmint" and version.startswith("19")):
         os_name = "linux-gnu-ubuntu-18.04"
     elif (distname == "ubuntu" and version.startswith("20")):
