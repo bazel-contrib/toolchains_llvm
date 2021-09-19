bazel-contrib/toolchains_llvm

commit cb7bc9e3b0a7bc9d377a1fc7b86fe8d855b2c64d

Restore Windows bypass
This was deleted in the previous commit.

toolchain/internal/configure.bzl
@@ -41,10 +41,19 @@ def _make_shortos(x):
         return "linux"
     elif x == "mac os x":
         return "darwin"
+    elif rctx.os.name.startswith("windows"):
+        return "windows"
     fail("Unsupported OS: " + x)
 
 def llvm_toolchain_impl(rctx):
     shortos = _make_shortos(rctx.os.name)
+    if shortos == "windows":
+        rctx.file("BUILD")
+        rctx.file("toolchains.bzl", """
+def llvm_register_toolchains():
+    pass
+        """)
+        return
 
     repo_path = str(rctx.path(""))
     relative_path_prefix = "external/%s/" % rctx.name

commit 2eabab0a2006d9df6b16b24fea59031c1b5f6b6d

Fix error in OS name normalization
This was introduced in cb7bc9e.

toolchain/internal/configure.bzl
@@ -41,7 +41,7 @@ def _make_shortos(x):
         return "linux"
     elif x == "mac os x":
         return "darwin"
-    elif rctx.os.name.startswith("windows"):
+    elif x.startswith("windows"):
         return "windows"
     fail("Unsupported OS: " + x)
 