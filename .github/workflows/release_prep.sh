e4fad4e6c1821d805ffd9c21bdd6e2cc52764862
Siddhartha Bagaria <starsid@gmail.com>
2023-09-14 09:06:35 +0000

    Automatically update versions in MODULE.bazel

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -2,13 +2,17 @@
 
 set -o errexit -o nounset -o pipefail
 
+git config user.email "you@example.com"
+git config user.name "Your Name"
+
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 tag=${GITHUB_REF_NAME}
 # The prefix is chosen to match what GitHub generates for source archives
 prefix="toolchains_llvm-${tag}"
 archive="toolchains_llvm-${tag}.tar.gz"
-git archive --format=tar --prefix="${prefix}/" "${tag}" | gzip >"${archive}"
+sed -i.bak "s/0.0.0/${tag}/" MODULE.bazel && git add MODULE.bazel && git commit -m "Update version" >/dev/null
+git archive --format=tar --prefix="${prefix}/" HEAD | gzip >"${archive}"
 sha=$(shasum -a 256 "${archive}" | cut -f1 -d' ')
 
 sed \
