5a276de87bc38e6f2e2686b287e27b3191e39065
Siddhartha Bagaria <starsid@gmail.com>
2023-09-19 16:06:05 +0000

    Instructions for git_override in release notes

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -7,7 +7,8 @@ git config user.name "Your Name"
 
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-tag=${GITHUB_REF_NAME}
+tag="${GITHUB_REF_NAME}"
+commit="${GITHUB_SHA}"
 # The prefix is chosen to match what GitHub generates for source archives
 prefix="toolchains_llvm-${tag}"
 archive="toolchains_llvm-${tag}.tar.gz"
@@ -17,6 +18,7 @@ sha=$(shasum -a 256 "${archive}" | cut -f1 -d' ')
 
 sed \
   -e "s/{tag}/${tag}/g" \
+  -e "s/{commit}/${commit}/g" \
   -e "s/{prefix}/${prefix}/g" \
   -e "s/{archive}/${archive}/g" \
   -e "s/{sha}/${sha}/g" \
