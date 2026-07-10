1c11ea7c964d988ada265e9f4d89dc1fb1100ad5
Rahul Butani <7833358+rrbutani@users.noreply.github.com>
2026-07-10 11:56:03 -0700

    ci: update `libtinfo5` URL

--- tests/scripts/ubuntu_install_libtinfo.sh
+++ tests/scripts/ubuntu_install_libtinfo.sh
@@ -13,5 +13,5 @@ set -euo pipefail
 pkg="$(mktemp --suffix=.deb)"
 trap 'rm -f "${pkg}"' EXIT
 
-curl -L https://mirrors.kernel.org/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2ubuntu0.1_amd64.deb -o "${pkg}"
+curl -L https://mirrors.kernel.org/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2ubuntu0.2_amd64.deb -o "${pkg}"
 sudo dpkg -i "${pkg}"
